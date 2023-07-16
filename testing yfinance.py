9fdc975f92fc70ae8158183a57b1f76dcba507a2
hwyld <134890421+hwyld@users.noreply.github.com>
2023-07-16 12:31:52 +1000

    Working (#1)
    * plotting
    
    * portfolio update
    
    * adding changes to input trades made throughout the year

--- testing yfinance.py
+++ testing yfinance.py
@@ -1,7 +1,82 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Import yfinance module in the program  
-import yfinance as yahooFin  
-# Using ticker for the Facebook in yfinance function  
-retrFBInfo = yahooFin.Ticker("FB")  
-# Printing Facebook financial information in the output  
-print(retrFBInfo.info)  
+msft = yf.Ticker("MSFT")
+
+# get all stock info
+msft.info
+
+# get historical market data
+hist = msft.history(period="1mo")
+
+print(hist)
+
+
+# show meta information about the history (requires history() to be called first)
+msft.history_metadata
+
+# show actions (dividends, splits, capital gains)
+msft.actions
+msft.dividends
+msft.splits
+msft.capital_gains  # only for mutual funds & etfs
+
+# show share count
+msft.get_shares_full(start="2022-01-01", end=None)
+
+# show financials:
+# - income statement
+msft.income_stmt
+msft.quarterly_income_stmt
+# - balance sheet
+msft.balance_sheet
+msft.quarterly_balance_sheet
+# - cash flow statement
+msft.cashflow
+msft.quarterly_cashflow
+# see `Ticker.get_income_stmt()` for more options
+
+# show holders
+msft.major_holders
+msft.institutional_holders
+msft.mutualfund_holders
+
+# Show future and historic earnings dates, returns at most next 4 quarters and last 8 quarters by default. 
+# Note: If more are needed use msft.get_earnings_dates(limit=XX) with increased limit argument.
+msft.earnings_dates
+
+# show ISIN code - *experimental*
+# ISIN = International Securities Identification Number
+msft.isin
+
+# show options expirations
+msft.options
+
+# show news
+msft.news
+
+# get option chain for specific expiration
+opt = msft.option_chain('2023-07-14')
+# data available via: opt.calls, opt.puts
+
+data = yf.download("SPY AAPL", period="1mo")
+
+
+# Assuming your data is in a pandas DataFrame format
+# your_data = ...
+
+# Export to CSV
+data.to_csv('data.csv')
+
+# Plot the data
+plt.figure(figsize=(14, 7))
+for column in data.columns:
+    plt.plot(data.index, data['Adj Close'], label='Adjusted Close')
+
+plt.title('Title of your chart')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.grid(True)
+plt.show()
