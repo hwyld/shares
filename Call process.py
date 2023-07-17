cc74c42794432464ebdf33fd575f940f52177737
hwyld <henry_wyld@hotmail.com>
2023-07-17 16:03:02 +1000

    modify call process

--- Call process.py
+++ Call process.py
@@ -1,14 +1,14 @@
 # CALL ENTIRE PORTFOLIO UPDATE FUNCTION
 
-import os
+#import os
 
 # Import portfolio and trades
-os.system("Portfolio and trade import.py")
+#os.system("Portfolio and trade import.py")
 
 # Import cash balances and recorded dividends
-os.system("Cash balances.py")
+#os.system("Cash balances.py")
 
 # Run yfinance import for live market prices and dividends + 
-os.system("yfinance import.py")
+#os.system("yfinance import.py")
 
 # Run charting package.py
